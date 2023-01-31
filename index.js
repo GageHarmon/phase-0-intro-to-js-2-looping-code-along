51af0cfcdcc62d554400a6e6f24f2b683ee8d823
Gage Harmon <gaharmon1@gmail.com>
2023-01-31 11:06:45 -0700

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+let cards = ["Guadalupe", "Ollie", "Aki"];
+const newCards = [];
+
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+//    console.log(cards);
+    newCards.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+  }
+  return newCards;
+}
+
+let number = [];
+
+function countDown(number) {
+  let i = 10;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
+
+
+
+
